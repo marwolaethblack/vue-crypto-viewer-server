6ceeefef061676d3b008f6d7e703442ed486a657
Adnan Unal <a.unal677@gmail.com>
2017-10-22 14:40:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var compression = require('compression');
+var morgan = require('morgan');
+var axios = require('axios');
+
+
+
+var app = express();
+app.use(morgan('combined'));
+app.use(compression());
+
+app.get('/', function (req, res) {
+    axios.get('https://coinbin.org/lbc')
+        .then(function (response) {
+            res.json(response.data);
+        })
+        .catch(function (error) {
+
+        });
+})
+
+
+app.listen(3000, function() {
+    console.log('Server is listening on localhost:3000');
+})
