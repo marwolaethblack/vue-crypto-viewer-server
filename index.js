c26bab00da315b900bd892a545c1eeb7665254d5
Adnan Unal <a.unal677@gmail.com>
2017-11-01 19:56:39 +0100

    add dynamic currency, exchange and types of history data route

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var morgan = require('morgan');
 var axios = require('axios');
 
 
-
 var app = express();
 app.use(compression());
 app.use(morgan('combined'));
+app.set('port', process.env.PORT || 3110);
 
 
 
 app.get('/api/coins/top', function(req,res) {
-    axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+
+    var currency = req.query.currency || 'USD';
+
+    axios.get(`https://api.coinmarketcap.com/v1/ticker/?limit=10&convert=${currency}`)
         .then(function (response) {
             res.status(200).json(response.data);
         })
@@ -27,7 +30,45 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/coins/:coin/history', function (req, res) {
-    axios.get(`https://min-api.cryptocompare.com/data/histoday?fsym=${req.params.coin}&tsym=USD&limit=30&&e=CCCAGG`)
+
+    var apiURL = 'https://min-api.cryptocompare.com/data/';
+    var coin = req.params.coin || 'BTC';
+    var exchange = req.query.e || 'CCCAGG';
+    var currency = req.query.currency || 'USD';
+    var type = req.query.type;
+
+    switch(type) {
+        case '24h': {
+            apiURL += `histohour?fsym=${coin}&tsym=${currency}&limit=24&e=${exchange}`;
+        }
+
+        case 'month': {
+            apiURL += `histoday?fsym=${coin}&tsym=${currency}&limit=30&e=${exchange}`;
+            break;
+        }
+
+        case '6months': {
+            apiURL += `histoday?fsym=${coin}&tsym=${currency}&aggregate=7&limit=27&e=${exchange}`;
+            break;
+        }
+
+        case 'year': {
+            apiURL += `histoday?fsym=${coin}&tsym=${currency}&aggregate=10&limit=37&e=${exchange}`;
+            break;
+        }
+
+        case 'all': {
+            apiURL += `histoday?fsym=${coin}&tsym=${currency}&aggregate=30&e=${exchange}&allData=true`;
+        }
+
+        default: {
+            apiURL += `histoday?fsym=BTC&tsym=USD&limit=30&e=CCCAGG`;
+            break;
+        }
+    }
+    console.log(apiURL);
+
+    axios.get(apiURL)
         .then(function (response) {
             res.status(200).json(response.data);
         })
@@ -36,6 +77,6 @@ app.get('/api/coins/:coin/history', function (req, res) {
         })
 });
 
-app.listen(3110, function() {
-    console.log('Server is listening on localhost:3110');
+app.listen(app.get('port'), function() {
+    console.log(`Server is listening on localhost:${app.get('port')}`);
 });
