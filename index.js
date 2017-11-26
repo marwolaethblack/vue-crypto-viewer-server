514e64bd55533b079fb7aba931e5d2e2954fce53
marwolaethblack <a.unal677@gmail.com>
2017-11-26 17:18:10 +0100

    a

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var compression = require('compression');
-var morgan = require('morgan');
 
 
 var routes = require('./routes/router');
@@ -9,16 +8,15 @@ var routes = require('./routes/router');
 
 var app = express();
 app.use(compression());
-app.use(morgan('combined'));
 app.set('port', process.env.PORT || 3110);
 app.use(routes);
 
 // //For production testing
-// app.use(express.static(__dirname + "/dist")); //serves css files
-//
-// app.get('*', function(req,res) {
-//     res.render(',/dist/index.html');
-// });
+app.use(express.static(__dirname + "/dist")); //serves css files
+
+app.get('*', function(req,res) {
+    res.render('./dist/index.html');
+});
 
 app.listen(app.get('port'), function() {
     console.log(`Server is listening on localhost:${app.get('port')}`);
