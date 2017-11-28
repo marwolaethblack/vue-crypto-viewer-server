0f580d2549ff563ee6ac48df0782d4ec25315d2b
marwolaethblack <a.unal677@gmail.com>
2017-11-28 15:25:50 +0100

    remove prod testing comment

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ app.use(morgan('combined'));
 app.set('port', process.env.PORT || 3110);
 app.use(routes);
 
-// //For production testing
-// app.use(express.static(__dirname + "/dist")); //serves css files
-//
-// app.get('*', function(req,res) {
-//     res.render(',/dist/index.html');
-// });
 
 app.listen(app.get('port'), function() {
     console.log(`Server is listening on localhost:${app.get('port')}`);
